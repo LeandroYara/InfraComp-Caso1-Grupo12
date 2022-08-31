d30d7b73b2a02321c9c9244fd12219d633910d64
Edd1e <estebanrco@hotmail.com>
2022-08-31 12:38:15 -0500

    implementación ocn errores

--- src/App.java
+++ src/App.java
@@ -1,8 +1,8 @@
 public class App{
     private static boolean bol=false;
     public static void main(String[] args) {
-        int numMensajes=100;
-        int tamanoBuzon=1;
+        int numMensajes=10000;
+        int tamanoBuzon=2;
         Buzon buzonInicial=new Buzon(tamanoBuzon, 0);
        
 
